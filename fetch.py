90013bc75607cce0ca3d3e53663dba4b21b1a8e2
jioodiff <muhammadzio04@gmail.com>
2026-07-31 22:41:51 +0700

    chore: remove unused temporary scripts (check_all_images.php, fetch.py)

--- fetch.py
+++
@@ -1,8 +0,0 @@
-import urllib.request, re
-try:
-    html = urllib.request.urlopen('https://id.wikipedia.org/wiki/Universitas_Islam_Negeri_Syarif_Hidayatullah_Jakarta').read().decode('utf-8')
-    urls = re.findall(r'https://upload.wikimedia.org/wikipedia/commons/[^\"]+\.jpg', html)
-    for url in urls:
-        print(url)
-except Exception as e:
-    print(e)
